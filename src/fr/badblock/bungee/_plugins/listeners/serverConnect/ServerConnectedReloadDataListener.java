8afc39a491ec82b8622fc45e84beec4b9533f02c
RedSpri <thereddev.redspri@gmail.com>
2018-02-18 15:04:22 +0100

    fix

--- src/fr/badblock/bungee/_plugins/listeners/serverConnect/ServerConnectedReloadDataListener.java
+++ src/fr/badblock/bungee/_plugins/listeners/serverConnect/ServerConnectedReloadDataListener.java
@@ -2,7 +2,7 @@ package fr.badblock.bungee._plugins.listeners.serverConnect;
 
 import fr.badblock.bungee._plugins.listeners.BadListener;
 import fr.badblock.bungee.players.BadPlayer;
-import fr.toenga.common.utils.data.Callback;
+import fr.badblock.bungee.utils.ThreadRunnable;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.ServerConnectedEvent;
 import net.md_5.bungee.event.EventHandler;
@@ -14,15 +14,11 @@ public class ServerConnectedReloadDataListener extends BadListener
 	@EventHandler (priority = EventPriority.LOW)
 	public void onServerConnected(ServerConnectedEvent event)
 	{
-		ProxiedPlayer proxiedPlayer = event.getPlayer();
-		BadPlayer badPlayer = BadPlayer.get(proxiedPlayer);
-		badPlayer.registerLoadedCallback(new Callback<BadPlayer>()
-		{
-			@Override
-			public void done(BadPlayer result, Throwable error) {
-				badPlayer.updateLastServer(proxiedPlayer);	
-			}
-		});
-	}
+        ThreadRunnable.run(() -> {
+            ProxiedPlayer proxiedPlayer = event.getPlayer();
+            BadPlayer badPlayer = BadPlayer.get(proxiedPlayer);
+            badPlayer.updateLastServer(proxiedPlayer);
+        });
+    }
 
 }
