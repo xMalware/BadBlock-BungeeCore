13740b52ae901cb50f1b7db4674d6c8acc853e73
xMalware <xmalware2@gmail.com>
2018-05-26 01:56:29 +0200

    Added comments for the following commands: brohoof

--- src/fr/badblock/bungee/_plugins/commands/z_basic/BrohoofCommand.java
+++ src/fr/badblock/bungee/_plugins/commands/z_basic/BrohoofCommand.java
@@ -4,15 +4,29 @@ import fr.badblock.bungee._plugins.commands.BadCommand;
 import fr.badblock.bungee.utils.i18n.I19n;
 import net.md_5.bungee.api.CommandSender;
 
+/**
+ * 
+ * This command is a reference to My Little Pony.
+ * Usage: /brohoof
+ * 
+ * @author xMalware
+ *
+ */
 public class BrohoofCommand extends BadCommand
 {
 
-	public BrohoofCommand() {
+	/**
+	 * Command constructor
+	 */
+	public BrohoofCommand()
+	{
 		super("brohoof", "", ")");
 	}
 
 	@Override
-	public void run(CommandSender sender, String[] args) {
+	public void run(CommandSender sender, String[] args)
+	{
+		// Brohoof!
 		I19n.sendMessage(sender, "commands.broohof.message", sender.getName());
 	}
 
