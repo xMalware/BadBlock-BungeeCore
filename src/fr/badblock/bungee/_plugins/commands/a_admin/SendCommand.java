c5701ffec1862c4948d9c714e27f0ed13fa3b96f
xMalware <xmalware2@gmail.com>
2018-05-26 01:51:33 +0200

    Added comments for the following commands: send

--- src/fr/badblock/bungee/_plugins/commands/a_admin/SendCommand.java
+++ src/fr/badblock/bungee/_plugins/commands/a_admin/SendCommand.java
@@ -10,47 +10,73 @@ import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.config.ServerInfo;
 
 /**
+ * 
  * Command to send a player from one server to another from the same network.
  * This command works across the network and therefore through any bungee connected to the same servers and the same production mode.
- * @author root
+ * 
+ * The required permission to execute this command is: bungee.command.send
+ * 
+ * @author xMalware
  *
  */
 public class SendCommand extends BadCommand
 {
-
-	public SendCommand() {
+	
+	/**
+	 * Command constructor
+	 */
+	public SendCommand()
+	{
 		super("send", "bungee.command.send", "move");
 	}
-
+	
+	/**
+	 * Method called when using the command
+	 */
 	@Override
-	public void run(CommandSender sender, String[] args) {
+	public void run(CommandSender sender, String[] args)
+	{
+
+		// If no argument has been entered, i.e. the user name and the server name
 		if (args.length != 2)
 		{
+			// A message is sent to him containing the information allowing him to take note of the use of the command.
 			I19n.sendMessage(sender, "commands.send.usage");
+			// Nothing has been written from him, no argument. After we explain it to him, we stop there.
 			return;
 		}
 
+		// We get the nickname to send to another server
 		String playerName = args[0];
 		
-		// Disconnected
+		// If the player is disconnected from the network
 		if (!BungeeManager.getInstance().hasUsername(playerName))
 		{
+			// In this case, we notify the user who executed this command
 			I19n.sendMessage(sender, "commands.send.disconnected", playerName);
+			// We stop there.
 			return;
 		}
 		
+		// We get the name of the server
 		String serverName = args[1];
+		// We get the information of the server
 		ServerInfo serverInfo = ProxyServer.getInstance().getServerInfo(serverName);
 		
+		// If there is no information about the server
 		if (serverInfo == null)
 		{
+			// In this case, we notify the user who executed this command
 			I19n.sendMessage(sender, "commands.send.unknownserver", serverName);
+			// We stop there.
 			return;
 		}
 		
-		// TODO hierarchy
+		// TODO Establish a hierarchy system. Not being able to send a player with higher permissions
 		
+		// We send the player's move with a packet over the network
 		BungeeManager.getInstance().sendPacket(new PlayerPacket(playerName, PlayerPacketType.SEND_SERVER, serverName));
+		// The requester is told that the action was indeed made
 		I19n.sendMessage(sender, "commands.send.sended", playerName, serverName);
 	}
 
