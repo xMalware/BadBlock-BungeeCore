f445473520037114e1330c9cf9dc297529bd8503
xMalware <xmalware2@gmail.com>
2018-06-07 02:03:10 +0200

    Added token query

--- src/fr/badblock/bungee/rabbit/claimants/BungeeTokenQuery.java
+++ src/fr/badblock/bungee/rabbit/claimants/BungeeTokenQuery.java
@@ -1,5 +1,13 @@
 package fr.badblock.bungee.rabbit.claimants;
 
+import fr.badblock.api.common.tech.rabbitmq.RabbitService;
+import fr.badblock.api.common.tech.rabbitmq.packet.RabbitPacket;
+import fr.badblock.api.common.tech.rabbitmq.packet.RabbitPacketEncoder;
+import fr.badblock.api.common.tech.rabbitmq.packet.RabbitPacketMessage;
+import fr.badblock.api.common.tech.rabbitmq.packet.RabbitPacketType;
+import fr.badblock.bungee.BadBungee;
+import fr.badblock.bungee.rabbit.BadBungeeQueues;
+
 /**
  * 
  * Token claimant
@@ -24,7 +32,18 @@ public class BungeeTokenQuery extends RabbitClaimant
 	@Override
 	public void run()
 	{
-		// TODO
+		// Get the main class
+		BadBungee badBungee = BadBungee.getInstance();
+		// Get the rabbit service
+		RabbitService rabbitService = badBungee.getRabbitService();
+		
+		// Create a new rabbit packet message
+		RabbitPacketMessage rabbitPacketMessage = new RabbitPacketMessage(-1, "");
+		// Create a rabbit packet
+		RabbitPacket rabbitPacket = new RabbitPacket(rabbitPacketMessage, BadBungeeQueues.BUNGEE_TOKEN_QUERY, true, RabbitPacketEncoder.UTF8, RabbitPacketType.PUBLISHER);
+		
+		// Send the packet
+		rabbitService.sendPacket(rabbitPacket);
 	}
 	
 }
