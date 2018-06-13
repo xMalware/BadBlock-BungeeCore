fc377a27457a6923a2fb17f2332ce5fc4f70cbe8
xMalware <xmalware2@gmail.com>
2018-06-13 07:17:04 +0200

    ChatColor utils comments

--- src/fr/badblock/bungee/utils/ChatColorUtils.java
+++ src/fr/badblock/bungee/utils/ChatColorUtils.java
@@ -11,15 +11,32 @@ import net.md_5.bungee.api.ChatColor;
  */
 public class ChatColorUtils {
 
+	/**
+	 * Translate colors
+	 * @param character
+	 * @param message
+	 * @return
+	 */
 	public static String translateColors(char character, String message) {
+		// Use chatcolor API
 		return ChatColor.translateAlternateColorCodes(character, message);
 	}
 
+	/**
+	 * Translate colors of messages
+	 * @param character
+	 * @param messages
+	 * @return
+	 */
 	public static String[] translateColors(char character, String... messages) {
+		// Create a new array
 		String[] result = new String[messages.length];
+		// For each message
 		for (int i = 0; i < messages.length; i++) {
+			// Translate the color
 			result[i] = translateColors(character, messages[i]);
 		}
+		// Returns the array
 		return result;
 	}
 
