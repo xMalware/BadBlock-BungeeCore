8a6bddea5f08b42a8442b2e2f207402837a53e21
xMalware <xmalware2@gmail.com>
2018-06-13 07:00:24 +0200

    Added ban reason

--- src/fr/badblock/bungee/modules/commands/modo/objects/BanReason.java
+++ src/fr/badblock/bungee/modules/commands/modo/objects/BanReason.java
@@ -1,40 +1,39 @@
 package fr.badblock.bungee.modules.commands.modo.objects;
 
-public enum BanReason {
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
 
-	CHEATING,
+import com.google.gson.reflect.TypeToken;
+import com.mongodb.DBObject;
 
-	COMPROMISED_ACCOUNT,
+import fr.badblock.api.common.utils.GsonUtils;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
-	ADVERTISING,
+@AllArgsConstructor
+@Data
+public class BanReason
+{
+	
+	public static final Type collectionType = new TypeToken<TreeMap<String, DBObject>>() {}.getType();
 
-	INAPPROPRIATE_NAME_OR_SKIN,
-
-	INAPPROPRIATE_BEHAVIOUR,
-
-	VERBAL_ABUSE,
-
-	BAN_EVASION,
-
-	FRAUD,
-
-	HACK_THREATS,
-
-	ANTI_GAMING,
-
-	EXPLOIT;
-
-	public String getName() {
-		return name().toLowerCase();
-	}
-
-	public static BanReason getFromString(String string) {
-		for (BanReason banReason : values()) {
-			if (banReason.name().equalsIgnoreCase(string)) {
-				return banReason;
-			}
+	private String					name;
+	private Map<String, BanIndex>	punishments;
+	
+	public BanReason(DBObject dbObject)
+	{
+		name = dbObject.get("name").toString();
+		punishments = new HashMap<>();
+		
+		Map<String, DBObject> temp = GsonUtils.getGson().fromJson(dbObject.get("punishments").toString(), collectionType);
+		
+		for (Entry<String, DBObject> entry : temp.entrySet())
+		{
+			punishments.put(entry.getKey(), new BanIndex(entry.getValue()));
 		}
-		return null;
 	}
-
+	
 }
