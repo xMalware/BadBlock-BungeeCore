e295a18b63944182ce0512b89fe9619924bdf970
xMalware <xmalware2@gmail.com>
2018-06-13 07:01:51 +0200

    Added comments / BanIndex

--- src/fr/badblock/bungee/modules/commands/modo/objects/BanIndex.java
+++ src/fr/badblock/bungee/modules/commands/modo/objects/BanIndex.java
@@ -7,15 +7,39 @@ import lombok.Data;
 
 @AllArgsConstructor
 @Data
+/**
+ * 
+ * Ban index
+ * 
+ * @author xMalware
+ *
+ */
 public class BanIndex
 {
 
+	/**
+	 * Index
+	 * @param Set the new index
+	 * @return Returns the current index
+	 */
 	private	int		index;
+	
+	/**
+	 * Time
+	 * @param Set the new time
+	 * @return Returns the current time
+	 */
 	private long	time;
 	
+	/**
+	 * Constructor
+	 * @param Database object
+	 */
 	public BanIndex(DBObject dbObject)
 	{
+		// Set index
 		index = Integer.parseInt(dbObject.get("index").toString());
+		// Set time
 		time = Long.parseLong(dbObject.get("time").toString());
 	}
 	
