d2747dcf6f78c8cf1b297bed8e9e1286c7a3ad8b
xMalware <xmalware2@gmail.com>
2018-06-14 03:18:21 +0200

    Added command: connectserver

---
+++ src/fr/badblock/bungee/modules/commands/modo/ConnectServerCommand.java
@@ -0,0 +1,71 @@
+package fr.badblock.bungee.modules.commands.modo;
+
+import fr.badblock.bungee.modules.commands.BadCommand;
+import fr.badblock.bungee.utils.i18n.I19n;
+import net.md_5.bungee.BungeeCord;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.config.ServerInfo;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+
+/**
+ * 
+ * Connect player command
+ *
+ * @author xMalware
+ *
+ */
+public class ConnectServerCommand extends BadCommand {
+
+	// I18n key prefix
+	private String prefix = "bungee.commands.connectserver.";
+
+	/**
+	 * Command constructor
+	 */
+	public ConnectServerCommand() {
+		// Super!
+		super("connectserver", null, "cos");
+		this.setForPlayersOnly(true);
+	}
+
+	/**
+	 * Method called when using the command
+	 */
+	@Override
+	public void run(CommandSender sender, String[] args) {
+		// If no argument has been entered
+		if (args.length <= 0) {
+			// We give him help.
+			help(sender);
+			// We stop there.
+			return;
+		}
+
+		String serverName = args[0];
+
+		ServerInfo serverInfo = BungeeCord.getInstance().getServerInfo(serverName);
+		
+		if (serverInfo == null)
+		{
+			I19n.sendMessage(sender, prefix + "unknownserver", null);
+			return;
+		}
+		
+		ProxiedPlayer proxiedPlayer = (ProxiedPlayer) sender;
+		
+		proxiedPlayer.connect(serverInfo);
+		
+		I19n.sendMessage(sender, prefix + "teleported", null, serverInfo.getName());
+	}
+
+	/**
+	 * Send help to the player
+	 * 
+	 * @param sender
+	 */
+	private void help(CommandSender sender) {
+		// Send help
+		I19n.sendMessage(sender, prefix + "help", null);
+	}
+
+}
