1448cfcdbbf88de697ddb61db6ba05b2999f53a0
xMalware <xmalware2@gmail.com>
2018-06-14 03:43:48 +0200

    Added forcekick command (in case of a player problem that remains in cache @micro_maniaque)

---
+++ src/fr/badblock/bungee/link/processing/bungee/BungeeForceKickProcessing.java
@@ -0,0 +1,52 @@
+package fr.badblock.bungee.link.processing.bungee;
+
+import fr.badblock.api.common.sync.bungee._BungeeProcessing;
+import fr.badblock.api.common.sync.bungee.packets.BungeePacketType;
+import fr.badblock.bungee.modules.commands.admin.ForceKickCommand;
+import fr.badblock.bungee.players.BadPlayer;
+import fr.badblock.bungee.utils.i18n.I19n;
+import net.md_5.bungee.BungeeCord;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+
+/**
+ * 
+ * Force kick BungeeCord packet
+ * 
+ * @author root
+ *
+ */
+public class BungeeForceKickProcessing extends _BungeeProcessing {
+
+	@SuppressWarnings("deprecation")
+	@Override
+	/**
+	 * 
+	 * Bungee bad player processing
+	 * 
+	 * @param Messages
+	 *
+	 */
+	public void done(String message) {
+		// Get the BadPlayer object
+		BadPlayer badPlayer = BadPlayer.get(message);
+		// If the bad player is null
+		if (badPlayer == null)
+		{
+			// Returns null
+			return;
+		}
+		ProxiedPlayer proxiedPlayer = BungeeCord.getInstance().getPlayer(badPlayer.getName());
+		if (proxiedPlayer != null)
+		{
+			proxiedPlayer.disconnect(I19n.getMessage(proxiedPlayer, ForceKickCommand.prefix + "youhavebeenkicked", null));
+		}
+		// Remove the bad player
+		badPlayer.remove();
+	}
+
+	@Override
+	public BungeePacketType getPacketType() {
+		return BungeePacketType.LOG;
+	}
+
+}
