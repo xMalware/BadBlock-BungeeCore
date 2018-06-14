452b845dd83e5df508bbc9a05ff0224128266896
xMalware <xmalware2@gmail.com>
2018-06-14 03:18:27 +0200

    Added command: connectplayer

---
+++ src/fr/badblock/bungee/modules/commands/modo/ConnectPlayerCommand.java
@@ -0,0 +1,97 @@
+package fr.badblock.bungee.modules.commands.modo;
+
+import fr.badblock.bungee.link.bungee.BungeeManager;
+import fr.badblock.bungee.modules.commands.BadCommand;
+import fr.badblock.bungee.players.BadPlayer;
+import fr.badblock.bungee.utils.i18n.I19n;
+import net.md_5.bungee.BungeeCord;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.config.ServerInfo;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+
+/**
+ * 
+ * Connect player command
+ *
+ * @author xMalware
+ *
+ */
+public class ConnectPlayerCommand extends BadCommand {
+
+	// I18n key prefix
+	private String prefix = "bungee.commands.connectplayer.";
+
+	/**
+	 * Command constructor
+	 */
+	public ConnectPlayerCommand() {
+		// Super!
+		super("connectplayer", null, "cop");
+		this.setForPlayersOnly(true);
+	}
+
+	/**
+	 * Method called when using the command
+	 */
+	@Override
+	public void run(CommandSender sender, String[] args) {
+		// If no argument has been entered
+		if (args.length <= 0) {
+			// We give him help.
+			help(sender);
+			// We stop there.
+			return;
+		}
+
+		String playerName = args[0];
+
+		BungeeManager bungeeManager = BungeeManager.getInstance();
+		
+		if (!bungeeManager.hasUsername(playerName))
+		{
+			I19n.sendMessage(sender, prefix + "offline", null);
+			return;
+		}
+		
+		BadPlayer badPlayer = bungeeManager.getBadPlayer(playerName);
+		
+		if (badPlayer == null)
+		{
+			I19n.sendMessage(sender, prefix + "offline", null);
+			return;
+		}
+		
+		ProxiedPlayer proxiedPlayer = (ProxiedPlayer) sender;
+		
+		String currentServer = badPlayer.getCurrentServer();
+		
+		if (currentServer == null)
+		{
+			I19n.sendMessage(sender, prefix + "unknownserver", null);
+			return;
+		}
+		
+		ServerInfo serverInfo = BungeeCord.getInstance().getServerInfo(currentServer);
+		
+		if (serverInfo == null)
+		{
+			I19n.sendMessage(sender, prefix + "unknownserver", null);
+			return;
+		}
+		
+		proxiedPlayer.connect(serverInfo);
+		
+		I19n.sendMessage(sender, prefix + "teleported", null, badPlayer.getName(), serverInfo.getName());
+	}
+
+	/**
+	 * Send help to the player
+	 * 
+	 * @param sender
+	 */
+	private void help(CommandSender sender) {
+		// Send help
+		I19n.sendMessage(sender, prefix + "help", null);
+	}
+
+}
