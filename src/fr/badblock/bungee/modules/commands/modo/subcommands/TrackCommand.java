367a16931d98135e0b32b3226c71b0f07923dbc0
xMalware <xmalware2@gmail.com>
2018-06-16 21:35:33 +0200

    Track command fill

--- src/fr/badblock/bungee/modules/commands/modo/subcommands/TrackCommand.java
+++ src/fr/badblock/bungee/modules/commands/modo/subcommands/TrackCommand.java
@@ -1,7 +1,14 @@
 package fr.badblock.bungee.modules.commands.modo.subcommands;
 
+import fr.badblock.api.common.utils.data.Callback;
 import fr.badblock.bungee.link.bungee.BungeeManager;
+import fr.badblock.bungee.modules.commands.basic.friends.FriendListable;
+import fr.badblock.bungee.modules.commands.basic.msg.PMPrivacy;
+import fr.badblock.bungee.modules.commands.basic.party.Party;
+import fr.badblock.bungee.modules.commands.basic.party.PartyManager;
+import fr.badblock.bungee.modules.commands.basic.party.Partyable;
 import fr.badblock.bungee.modules.commands.modo.AbstractModCommand;
+import fr.badblock.bungee.players.BadIP;
 import fr.badblock.bungee.players.BadPlayer;
 import fr.badblock.bungee.utils.i18n.I19n;
 import net.md_5.bungee.api.CommandSender;
@@ -54,7 +61,39 @@ public class TrackCommand extends AbstractModCommand {
 			return;
 		}
 
+		String currentServer = badOnlinePlayer.getCurrentServer();
+		String bungeeGroups = badOnlinePlayer.getRawChatPrefix();
+		boolean isMute = badOnlinePlayer.getPunished() != null && badOnlinePlayer.getPunished().isMute();
+
+		FriendListable friendlistable = badOnlinePlayer.getSettings().getFriendListable();
+		Partyable partyable = badOnlinePlayer.getSettings().getPartyable();
+		PMPrivacy pmPrivacy = badOnlinePlayer.getSettings().getPmPrivacy();
+
+		BadIP badIp = BadIP.get(badOnlinePlayer.getLastIp());
 		
+		String locale = badOnlinePlayer.getLocale().name();
+		
+		PartyManager.getParty(playerName, new Callback<Party>()
+		{
+
+			@Override
+			public void done(Party result, Throwable error)
+			{
+				boolean inParty = result != null;
+				String rawParty = Boolean.toString(inParty);
+				String rawMute = Boolean.toString(isMute);
+				String countryName = badIp != null && badIp.getApiData() != null ? badIp.getApiData().getCountryName() : "unknown";
+				I19n.sendMessage(sender, getPrefix("message"), new int[] { 3, 4, 5, 6, 7, 8, 9 },
+						playerName, currentServer, bungeeGroups, getPrefix("mute_" + rawMute),
+						getPrefix("friendlistable_" + friendlistable.name()), getPrefix("partyable_" + partyable.name()),
+						getPrefix("pmprivacy_" + pmPrivacy.name()), "locale." + locale, getPrefix("party_" + rawParty),
+						"country." + countryName);
+			}
+
+		});
+
+
+
 	}
 
 }
