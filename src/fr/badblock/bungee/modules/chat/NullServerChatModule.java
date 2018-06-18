ab8da6d3cff2199626f616e959289db91aeaa185
xMalware <xmalware2@gmail.com>
2018-06-18 04:22:22 +0200

    Null server chat module

---
+++ src/fr/badblock/bungee/modules/chat/NullServerChatModule.java
@@ -0,0 +1,35 @@
+package fr.badblock.bungee.modules.chat;
+
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.connection.Connection;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.event.ChatEvent;
+
+public class NullServerChatModule extends ChatModule {
+
+	@Override
+	public ChatEvent check(ChatEvent event)
+	{
+		if (event.getSender() == null)
+		{
+			event.setCancelled(true);
+			return event;
+		}
+
+		Connection connection = event.getSender();
+
+		if (connection instanceof ProxiedPlayer) {
+			ProxiedPlayer bPlayer = (ProxiedPlayer) connection;
+			if (bPlayer != null && bPlayer.getServer() == null)
+			{
+				event.setCancelled(true);
+				bPlayer.disconnect(new TextComponent("Erreur. Impossible de trouver le serveur. (code 629)"));
+			}
+		}
+
+		return event;
+	}
+
+
+
+}
