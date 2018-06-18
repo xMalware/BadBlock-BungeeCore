ffda3b15c48bb9b55b066fec34fd89254918ff91
xMalware <xmalware2@gmail.com>
2018-06-18 04:51:05 +0200

    Load BadIP

---
+++ src/fr/badblock/bungee/modules/login/datamanager/PreLoginLoadIPListener.java
@@ -0,0 +1,69 @@
+package fr.badblock.bungee.modules.login.datamanager;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+
+import fr.badblock.bungee.link.bungee.BungeeManager;
+import fr.badblock.bungee.modules.abstracts.BadListener;
+import fr.badblock.bungee.players.BadIP;
+import net.md_5.bungee.api.connection.PendingConnection;
+import net.md_5.bungee.api.event.PreLoginEvent;
+import net.md_5.bungee.event.EventHandler;
+import net.md_5.bungee.event.EventPriority;
+
+/**
+ * 
+ * @author xMalware
+ *
+ */
+public class PreLoginLoadIPListener extends BadListener {
+
+	/**
+	 * When a player joins the server
+	 * 
+	 * @param event
+	 */
+	@EventHandler(priority = EventPriority.LOWEST)
+	public void onPreLogin(PreLoginEvent event) {
+		// If the connection attempt is cancelled first
+		if (event.isCancelled()) {
+			// So we stop there
+			return;
+		}
+
+		// Get bungee manager
+		BungeeManager bungeeManager = BungeeManager.getInstance();
+		// If the server ping null
+		if (bungeeManager.getServerPing() == null) {
+			// Generate the server ping to init slots/players values
+			// to avoid "this server is fucking full!"
+			bungeeManager.generatePing();
+		}
+
+		// Get pending connection
+		PendingConnection pendingConnection = event.getConnection();
+
+		if (pendingConnection == null)
+		{
+			return;
+		}
+
+		InetSocketAddress address = pendingConnection.getAddress();
+
+		if (address == null)
+		{
+			return;
+		}
+
+		InetAddress inetAddress = address.getAddress();
+
+		if (inetAddress == null)
+		{
+			return;
+		}
+
+		// We create a BadIP object
+		new BadIP(inetAddress.getHostAddress(), true);
+	}
+
+}
