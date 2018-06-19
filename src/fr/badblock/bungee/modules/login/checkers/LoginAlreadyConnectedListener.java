4e5ccc425465a45071e2fdcad29d217a21261a21
xMalware <xmalware2@gmail.com>
2018-06-20 00:13:55 +0200

    Already connected listener fix

--- src/fr/badblock/bungee/modules/login/checkers/LoginAlreadyConnectedListener.java
+++ src/fr/badblock/bungee/modules/login/checkers/LoginAlreadyConnectedListener.java
@@ -3,8 +3,9 @@ package fr.badblock.bungee.modules.login.checkers;
 import fr.badblock.api.common.utils.general.StringUtils;
 import fr.badblock.bungee.link.bungee.BungeeManager;
 import fr.badblock.bungee.modules.abstracts.BadListener;
-import fr.badblock.bungee.modules.login.events.PlayerJoinEvent;
-import fr.badblock.bungee.players.BadPlayer;
+import fr.badblock.bungee.players.BadOfflinePlayer;
+import net.md_5.bungee.api.connection.PendingConnection;
+import net.md_5.bungee.api.event.PreLoginEvent;
 import net.md_5.bungee.event.EventHandler;
 import net.md_5.bungee.event.EventPriority;
 
@@ -23,23 +24,24 @@ public class LoginAlreadyConnectedListener extends BadListener {
 	 * 
 	 * @param event
 	 */
-	@EventHandler(priority = EventPriority.LOW)
-	public void onPlayerJoinEvent(PlayerJoinEvent event) {
+	@EventHandler(priority = EventPriority.LOWEST)
+	public void onPlayerJoinEvent(PreLoginEvent event) {
 		if (event.isCancelled())
 		{
 			return;
 		}
-		// We get the BadPlayer object
-		BadPlayer badPlayer = event.getBadPlayer();
-		// We get his/her username
-		String username = badPlayer.getName();
+		// We get the PendingConnection object
+		PendingConnection connection = event.getConnection();
+		// We get the BadOfflinePlayer object
+		BadOfflinePlayer badOfflinePlayer = BadOfflinePlayer.get(connection.getName());
 		// We'll use our bungee manager to determinate if the player is already
 		// connected
 		BungeeManager bungeeManager = BungeeManager.getInstance();
 		// If the username is already registered in the sync data
-		if (bungeeManager.hasUsername(username)) {
+		if (bungeeManager.hasUsername(connection.getName())) {
 			// We cancel the player connection with a specific message
-			event.cancel(StringUtils.join(badPlayer.getTranslatedMessages("bungee.login.alreadyconnected", null), System.lineSeparator()));
+			event.setCancelled(true);
+			event.setCancelReason(StringUtils.join(badOfflinePlayer.getTranslatedMessages("bungee.login.alreadyconnected", null), System.lineSeparator()));
 		}
 	}
 
