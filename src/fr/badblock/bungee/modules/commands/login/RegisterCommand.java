cb31b7f25989eda1c9c64c36e9f5bad0e259687d
xMalware <xmalware2@gmail.com>
2018-06-22 10:09:32 +0200

    Added register command

---
+++ src/fr/badblock/bungee/modules/commands/login/RegisterCommand.java
@@ -0,0 +1,88 @@
+package fr.badblock.bungee.modules.commands.login;
+
+import fr.badblock.bungee.modules.commands.BadCommand;
+import fr.badblock.bungee.modules.login.datamanager.SkeletonConnectorListener;
+import fr.badblock.bungee.players.BadPlayer;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.config.ServerInfo;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+
+/**
+ * 
+ * @author xMalware
+ *
+ */
+public class RegisterCommand extends BadCommand {
+
+	private static String prefix = "bungee.commands.register.";
+
+	/**
+	 * Command constructor
+	 */
+	public RegisterCommand() {
+		super("register", "");
+		this.setForPlayersOnly(true);
+	}
+
+	/**
+	 * Method called when using the command
+	 */
+	@Override
+	public void run(CommandSender sender, String[] args) {
+		ProxiedPlayer proxiedPlayer = (ProxiedPlayer) sender;
+		BadPlayer badPlayer = BadPlayer.get(proxiedPlayer);
+
+		if (args.length != 2)
+		{
+			badPlayer.sendTranslatedOutgoingMessage(prefix + "usage", null);
+			return;
+		}
+
+		if (badPlayer.getLoginPassword() != null && !badPlayer.getLoginPassword().isEmpty())
+		{
+			badPlayer.sendTranslatedOutgoingMessage(prefix + "pleaselogin", null);
+			return;
+		}
+
+		String password = args[0];
+		String repeatPassword = args[0];
+
+		if (!HashLogin.isValidPassword(password))
+		{
+			badPlayer.sendTranslatedOutgoingMessage(prefix + "notsecureenough", null);
+			return;
+		}
+
+		if (!password.equals(repeatPassword))
+		{
+			badPlayer.sendTranslatedOutgoingMessage(prefix + "notsamepasswords", null);
+			return;
+		}
+
+		String passwordHash = HashLogin.hash(password);
+
+		badPlayer.setLoginPassword(passwordHash);
+		try
+		{
+			badPlayer.saveData();
+		}
+		catch (Exception exception)
+		{
+			exception.printStackTrace();
+		}
+		
+		ServerInfo server = SkeletonConnectorListener.roundrobinHub();
+
+		if (server == null)
+		{
+			badPlayer.sendTranslatedOutgoingMessage(prefix + "nohubavailable", null);
+			return;
+		}
+
+		badPlayer.sendTranslatedOutgoingMessage(prefix + "registered", null);
+		badPlayer.setLoginStep(true);
+
+		proxiedPlayer.connect(server);
+	}
+
+}
