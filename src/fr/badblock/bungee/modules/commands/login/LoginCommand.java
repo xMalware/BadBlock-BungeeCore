1dcc6391be4515b14abeec1b805c23ec083ccbed
xMalware <xmalware2@gmail.com>
2018-06-22 10:09:36 +0200

    Added login command

---
+++ src/fr/badblock/bungee/modules/commands/login/LoginCommand.java
@@ -0,0 +1,70 @@
+package fr.badblock.bungee.modules.commands.login;
+
+import fr.badblock.bungee.modules.commands.BadCommand;
+import fr.badblock.bungee.modules.login.datamanager.SkeletonConnectorListener;
+import fr.badblock.bungee.players.BadPlayer;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.config.ServerInfo;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+
+/**
+ * 
+ * @author xMalware
+ *
+ */
+public class LoginCommand extends BadCommand {
+
+	private static String prefix = "bungee.commands.login.";
+	
+	/**
+	 * Command constructor
+	 */
+	public LoginCommand() {
+		super("login", "", "l");
+		this.setForPlayersOnly(true);
+	}
+
+	/**
+	 * Method called when using the command
+	 */
+	@Override
+	public void run(CommandSender sender, String[] args) {
+		ProxiedPlayer proxiedPlayer = (ProxiedPlayer) sender;
+		BadPlayer badPlayer = BadPlayer.get(proxiedPlayer);
+		
+		if (args.length != 1)
+		{
+			badPlayer.sendTranslatedOutgoingMessage(prefix + "usage", null);
+			return;
+		}
+		
+		if (badPlayer.getLoginPassword() == null || badPlayer.getLoginPassword().isEmpty())
+		{
+			badPlayer.sendTranslatedOutgoingMessage(prefix + "pleaseregister", null);
+			return;
+		}
+		
+		String password = args[0];
+		
+		password = HashLogin.hash(password);
+		
+		if (!badPlayer.getLoginPassword().equals(password))
+		{
+			badPlayer.sendTranslatedOutgoingMessage(prefix + "invalidpassword", null);
+			return;
+		}
+		
+		ServerInfo server = SkeletonConnectorListener.roundrobinHub();
+		
+		if (server == null)
+		{
+			badPlayer.sendTranslatedOutgoingMessage(prefix + "nohubavailable", null);
+			return;
+		}
+		
+		badPlayer.setLoginStep(true);
+		badPlayer.sendTranslatedOutgoingMessage(prefix + "validpassword", null);
+		proxiedPlayer.connect(server);
+	}
+
+}
